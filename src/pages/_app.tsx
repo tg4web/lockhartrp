2113a3fc91900452eb0550fce3f147318ef3e6d7
troygilmore04 <103476955+troygilmore04@users.noreply.github.com>
2022-12-12 01:00:54 -0500

    ci: configured trpc backend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
+import type { AppType } from 'next/app';
 import '../styles/globals.css';
 import { trpc } from '../utils/trpc';
-import type { AppProps, AppType } from 'next/app';
 
 const App: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default trpc.withTRPC(App);
