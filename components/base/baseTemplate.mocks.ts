d2dc7c5f6819340ac3cd803997f6097ed6d82206
troygilmore04 <103476955+troygilmore04@users.noreply.github.com>
2022-12-11 21:30:58 -0500

    build: added a reusable components template

---
+++ components/base/baseTemplate.mocks.ts
@@ -0,0 +1,9 @@
+import { IBaseTemplate } from './baseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Sample Text',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
---
+++ components/cards/sampleCard/sampleCard.mocks.ts
@@ -0,0 +1,9 @@
+import { IBaseTemplate } from './sampleCard';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Sample Text',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
